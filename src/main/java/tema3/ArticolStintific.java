99c78abcf033adcd2cb58d0c05b4c9e3886eed3b
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-16 21:18:43 +0300

    tema3

--- src/main/java/tema3/ArticolStintific.java
+++ src/main/java/tema3/ArticolStintific.java
@@ -1,4 +1,37 @@
 package tema3;
 
-public class ArticolStintific {
+import java.util.List;
+
+public class ArticolStiintific implements Raport {
+    private String titlu;
+    private List<String> autori;
+    private List<Sectiune> sectiuni;
+    private List<String> bibliografie;
+
+    public ArticolStiintific(String titlu, List<String> autori, List<Sectiune> sectiuni, List<String> bibliografie) {
+        this.titlu = titlu;
+        this.autori = autori;
+        this.sectiuni = sectiuni;
+        this.bibliografie = bibliografie;
+    }
+
+    @Override
+    public String generareTitlu() {
+        return "Titlu: " + titlu + "\nAutori: " + String.join(", ", autori) + "\n";
+    }
+
+    @Override
+    public String generareContinut() {
+        StringBuilder continut = new StringBuilder();
+        for (Sectiune sectiune : sectiuni) {
+            continut.append("Secțiune: ").append(sectiune.getTitlu()).append("\n")
+                    .append(sectiune.getContinut()).append("\n");
+        }
+        return continut.toString();
+    }
+
+    @Override
+    public String generareIncheiere() {
+        return "Bibliografie:\n" + String.join("\n", bibliografie);
+    }
 }
--- src/main/java/tema3/RaportSedinta.java
+++ src/main/java/tema3/RaportSedinta.java
@@ -1,4 +1,47 @@
 package tema3;
 
-public class RaportSedinta {
+import java.util.Date;
+import java.util.List;
+
+public class RaportSedinta implements Raport {
+    private String motivSedinta;
+    private Date data;
+    private List<String> subiecteDiscutate;
+    private List<String> minuta;
+    private List<String> persoaneResponsabile;
+
+    public RaportSedinta(String motivSedinta, Date data, List<String> subiecteDiscutate, List<String> minuta, List<String> persoaneResponsabile) {
+        this.motivSedinta = motivSedinta;
+        this.data = data;
+        this.subiecteDiscutate = subiecteDiscutate;
+        this.minuta = minuta;
+        this.persoaneResponsabile = persoaneResponsabile;
+    }
+
+    @Override
+    public String generareTitlu() {
+        return "Motivul ședinței: " + motivSedinta + "\nData: " + data + "\n";
+    }
+
+    @Override
+    public String generareContinut() {
+        StringBuilder continut = new StringBuilder("Subiecte discutate:\n");
+        for (String subiect : subiecteDiscutate) {
+            continut.append("- ").append(subiect).append("\n");
+        }
+        continut.append("\nMinuta ședinței:\n");
+        for (String paragraf : minuta) {
+            continut.append(paragraf).append("\n");
+        }
+        return continut.toString();
+    }
+
+    @Override
+    public String generareIncheiere() {
+        return "Persoane responsabile pentru întocmirea raportului:\n" + String.join(", ", persoaneResponsabile);
+    }
+
+    public Date getData() {
+        return data;
+    }
 }
