5676b3b40a61e4103df4fdf65d24c4ef50afa521
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-21 13:42:36 +0300

    tema3

--- src/main/java/tema3/Sectiune.java
+++ src/main/java/tema3/Sectiune.java
@@ -1,9 +1,19 @@
 package tema3;
 
+/**
+ * @author Daniel Filip
+ * Aceasta clasa implementeaza atat titlul cat si continut unui articol,
+ */
+
 public class Sectiune {
     private String titlu;
     private String continut;
 
+    /**
+     *
+     * @param titlu titlul articolului
+     * @param continut continutul articolului
+     */
     public Sectiune(String titlu, String continut) {
         this.titlu = titlu;
         this.continut = continut;
