cc05fd3a8355ef677ac109e9c19373b13a6e2d04
Daniel Filip <filipdaniel2000@yahoo.com>
2024-10-21 13:45:10 +0300

    tema3

--- src/main/java/tema3/Raport.java
+++ src/main/java/tema3/Raport.java
@@ -1,5 +1,9 @@
 package tema3;
 
+/**
+ * @author Daniel Filip
+ * Interfata Raport care contine 3 declaratii care genereaza titlul, continutul si incheierea articolului
+ */
 public interface Raport {
     String generareTitlu();
     String generareContinut();
