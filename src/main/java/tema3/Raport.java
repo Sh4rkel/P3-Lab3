edc08bb048242f88069e027e66218513cfbd6cf0
Daniel-Marian Filip <99269936+Sh4rkel@users.noreply.github.com>
2024-11-02 23:48:54 +0200

    Update Raport.java

--- src/main/java/tema3/Raport.java
+++ src/main/java/tema3/Raport.java
@@ -4,6 +4,7 @@ package tema3;
  * @author Daniel Filip
  * Interfata Raport care contine 3 declaratii care genereaza titlul, continutul si incheierea articolului
  */
+
 public interface Raport {
     String generareTitlu();
     String generareContinut();
